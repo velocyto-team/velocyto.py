9ad3a5a5cda4e224b8b38223360ff42f3c892820
Gioele La Manno <gioelelamanno@gmail.com>
2018-02-07 11:05:13 +0100

    fix the docs

--- doc/_ext/edit_on_github.py
+++ doc/_ext/edit_on_github.py
@@ -42,7 +42,7 @@ def html_page_context(app, pagename, templatename, context, doctree):
     context['github_user'] = app.config.edit_on_github_project.split('/')[0]
     context['github_version'] = app.config.edit_on_github_branch + '/'
     context['github_repo'] = app.config.edit_on_github_project.split('/')[1]
-    context['source_suffix'] = app.config.source_suffix[0]
+    context['source_suffix'] = ".rst"  # app.config.source_suffix[0]
 
 
 def setup(app):
